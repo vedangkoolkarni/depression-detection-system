7887c150723cb51aee554b95c780ad2bf2705a4c
vedangkoolkarni <vedangkoolkarni@gmail.com>
2020-01-12 19:44:22 +0530

    corrected python code

--- python/src/main.py
+++ python/src/main.py
@@ -28,101 +28,111 @@ def main(image_path) :
   # image_path = sys.argv[1]
   # ? override path of image if you want to test other image here
   # image_path = '../images/happy.png'
-  detection_model_path = '../trained_models/detection_models/haarcascade_frontalface_default.xml'
-  # emotion_model_path = '../trained_models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
-  emotion_model_path = '../trained_models/emotion_models/fer2013_mini_XCEPTION.107-0.66.hdf5'
-  gender_model_path = '../trained_models/gender_models/simple_CNN.81-0.96.hdf5'
-  emotion_labels = get_labels('fer2013')
-  gender_labels = get_labels('imdb')
-  font = cv2.FONT_HERSHEY_SIMPLEX
+  
+  try :
+    print('starting to detect faces')
+    
+    detection_model_path = '../trained_models/detection_models/haarcascade_frontalface_default.xml'
+    # emotion_model_path = '../trained_models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
+    emotion_model_path = '../trained_models/emotion_models/fer2013_mini_XCEPTION.107-0.66.hdf5'
+    gender_model_path = '../trained_models/gender_models/simple_CNN.81-0.96.hdf5'
+    emotion_labels = get_labels('fer2013')
+    gender_labels = get_labels('imdb')
+    font = cv2.FONT_HERSHEY_SIMPLEX
 
-  # hyper-parameters for bounding boxes shape
-  gender_offsets = (30, 60)
-  gender_offsets = (10, 10)
-  emotion_offsets = (20, 40)
-  emotion_offsets = (0, 0)
+    # hyper-parameters for bounding boxes shape
+    gender_offsets = (30, 60)
+    gender_offsets = (10, 10)
+    emotion_offsets = (20, 40)
+    emotion_offsets = (0, 0)
 
-  # loading models
-  face_detection = load_detection_model(detection_model_path)
-  emotion_classifier = load_model(emotion_model_path, compile=False)
-  gender_classifier = load_model(gender_model_path, compile=False)
+    # loading models
+    face_detection = load_detection_model(detection_model_path)
+    emotion_classifier = load_model(emotion_model_path, compile=False)
+    gender_classifier = load_model(gender_model_path, compile=False)
 
-  # getting input model shapes for inference
-  emotion_target_size = emotion_classifier.input_shape[1:3]
-  gender_target_size = gender_classifier.input_shape[1:3]
-  # loading images
-  rgb_image = load_image(image_path, grayscale=False)
-  gray_image = load_image(image_path, grayscale=True)
-  gray_image = np.squeeze(gray_image)
-  gray_image = gray_image.astype('uint8')
+    # getting input model shapes for inference
+    emotion_target_size = emotion_classifier.input_shape[1:3]
+    gender_target_size = gender_classifier.input_shape[1:3]
+    # loading images
+    rgb_image = load_image(image_path, grayscale=False)
+    gray_image = load_image(image_path, grayscale=True)
+    gray_image = np.squeeze(gray_image)
+    gray_image = gray_image.astype('uint8')
 
-  faces = detect_faces(face_detection, gray_image)
-  face_type = str(type(faces))
-  if face_type == "<class 'numpy.ndarray'>" :
-    totalFaces = (faces.size / 4)
-    status = {
-      "status" : "face-detected-from-image",
-      "totalFaces": totalFaces
-    }
-    print(status)
-    if totalFaces == 1 :
-      for face_coordinates in faces:
-        x1, x2, y1, y2 = apply_offsets(face_coordinates, gender_offsets)
-        rgb_face = rgb_image[y1:y2, x1:x2]
+    print('starting to detect faces')
+    faces = detect_faces(face_detection, gray_image)
+    face_type = str(type(faces))
+    totalFaces = 0
+    print('initialized total faces')
 
-        x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
-        gray_face = gray_image[y1:y2, x1:x2]
-
-        try:
-            rgb_face = cv2.resize(rgb_face, (gender_target_size))
-            gray_face = cv2.resize(gray_face, (emotion_target_size))
-        except:
-            continue
-
-        rgb_face = preprocess_input(rgb_face, False)
-        rgb_face = np.expand_dims(rgb_face, 0)
-        gender_prediction = gender_classifier.predict(rgb_face)
-        gender_label_arg = np.argmax(gender_prediction)
-        gender_text = gender_labels[gender_label_arg]
-
-        gray_face = preprocess_input(gray_face, True)
-        gray_face = np.expand_dims(gray_face, 0)
-        gray_face = np.expand_dims(gray_face, -1)
-        emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
-        emotion_text = emotion_labels[emotion_label_arg]
-
-        if gender_text == gender_labels[0]:
-            color = (0, 0, 255)
-        else:
-            color = (255, 0, 0)
-
-        draw_bounding_box(face_coordinates, rgb_image, color)
-        draw_text(face_coordinates, rgb_image, gender_text, color, 0, -20, 1, 2)
-        draw_text(face_coordinates, rgb_image, emotion_text, color, 0, -50, 1, 2)
-
-      bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
-      cv2.imwrite('../images/predicted_test_image.png', bgr_image)
-
-      # print('the emotion in the picture is : ', emotion_text)
+    if face_type == "<class 'numpy.ndarray'>" :
+      totalFaces = (faces.size / 4)
       status = {
-        "status" : "emotion-detected-from-image",
-        "result" : emotion_text
+        "status" : "face-detected-from-image",
+        "totalFaces": totalFaces
       }
       print(status)
-      # print('the gender in the picture is : ', gender_text)
-    else:
+      if totalFaces == 1 :
+        for face_coordinates in faces:
+          x1, x2, y1, y2 = apply_offsets(face_coordinates, gender_offsets)
+          rgb_face = rgb_image[y1:y2, x1:x2]
+
+          x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
+          gray_face = gray_image[y1:y2, x1:x2]
+
+          try:
+              rgb_face = cv2.resize(rgb_face, (gender_target_size))
+              gray_face = cv2.resize(gray_face, (emotion_target_size))
+          except:
+              continue
+
+          rgb_face = preprocess_input(rgb_face, False)
+          rgb_face = np.expand_dims(rgb_face, 0)
+          gender_prediction = gender_classifier.predict(rgb_face)
+          gender_label_arg = np.argmax(gender_prediction)
+          gender_text = gender_labels[gender_label_arg]
+
+          gray_face = preprocess_input(gray_face, True)
+          gray_face = np.expand_dims(gray_face, 0)
+          gray_face = np.expand_dims(gray_face, -1)
+          emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
+          emotion_text = emotion_labels[emotion_label_arg]
+
+          if gender_text == gender_labels[0]:
+              color = (0, 0, 255)
+          else:
+              color = (255, 0, 0)
+
+          draw_bounding_box(face_coordinates, rgb_image, color)
+          draw_text(face_coordinates, rgb_image, gender_text, color, 0, -20, 1, 2)
+          draw_text(face_coordinates, rgb_image, emotion_text, color, 0, -50, 1, 2)
+
+        bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
+        cv2.imwrite('../images/predicted_test_image.png', bgr_image)
+
+        # print('the emotion in the picture is : ', emotion_text)
+        status = {
+          "status" : "emotion-detected-from-image",
+          "result" : emotion_text
+        }
+        print(status)
+        # print('the gender in the picture is : ', gender_text)
+      else:
+        status = {
+          "status" : "more-than-one-faces-detected",
+          "totalFaces": totalFaces
+        }
+        print(status)
+    else: 
       status = {
         "status" : "more-than-one-faces-detected",
         "totalFaces": totalFaces
       }
       print(status)
-  else: 
-    status = {
-      "status" : "more-than-one-faces-detected",
-      "totalFaces": totalFaces
-    }
-    print(status)
 
+  except Exception as e:
+    print(e)
   
 
 def captureImage():
@@ -190,7 +200,8 @@ if __name__ == "__main__":
   # print('image saved at : ', saved_image_path)
   # print('processing image to detect face and extract emotions')
   status = {
-    "status" : "processing-image"
+    "status" : "processing-image",
+    "image_path": saved_image_path
   }
   print(status)
   main(saved_image_path)
